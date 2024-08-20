8c5988eb5bea37da6ea9c4a40b8ab2dcdcb6fabb
Amina Ahamadi <amir974320@gmail.com>
2024-08-20 12:17:55 +0200

    stable version

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* Selected Section */
+const navButtons = document.querySelectorAll(".nav-buttons");
+function selected(button){
+    if(!button.classList.contains("active")){
+        button.classList.add("active");
+    }
+    navButtons.forEach( (element) => {
+        if(element.getAttribute("title") !== button.getAttribute("title") && element.classList.contains("active")){
+            element.classList.remove("active");
+        }
+    })
+}
+
+/* ======================= Typing animation ========================*/
+var typed = new Typed(".typing",{
+    strings:["Web Developer","Web Integrator","Front-End Developer","Full-Stack Developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
